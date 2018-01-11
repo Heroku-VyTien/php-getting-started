0bd148ccf21873d81d4f64ce5106ae582a828795
Tien Nguyen <mac-l011@mac-l011s-MacBook-Pro-2.local>
2018-01-11 15:34:43 +0700

    add sendmail.php

--- web/index.php
+++ web/index.php
@@ -2,6 +2,43 @@
 
 require('../vendor/autoload.php');
 
+
+//-------------------------- TEST MAIL ----------------------
+
+function Send_Mail($to,$subject,$body)
+{
+    require '/PHPMailer/class.phpmailer.php';
+    $from = "nguyen.tien@mulodo.com";
+    $mail = new PHPMailer();
+    $mail->IsSMTP(true); // SMTP
+    $mail->SMTPAuth   = true;  // SMTP authentication
+    $mail->Mailer = "smtp";
+    $mail->Host= "tls://email-smtp.us-east.amazonaws.com"; // Amazon SES
+    $mail->Port = 465;  // SMTP Port
+    $mail->Username = "AKIAIM3HS7UUXEQ4GTPA";  // SMTP  Username
+    $mail->Password = "AqF6E+ZsiPdZd+mvMhkRgiQvQH7tGH9Ky5POVQCG63qX";  // SMTP Password
+    $mail->SetFrom($from, 'Tien nguyen');
+    $mail->AddReplyTo($from,'Technical Support');
+    $mail->Subject = $subject;
+    $mail->MsgHTML($body);
+    $address = $to;
+    $mail->AddAddress($address, $to);
+    if(!$mail->Send())
+        return false;
+    else
+        return true;
+}
+
+$to = 'vytien@gmail.com';
+$subject = 'Test mail from AWS';
+$body = 'test body';
+$rst = Send_Mail($to,$subject,$body);
+if ($rst) echo "MAIL :: Success";
+else echo "MAIL :: Fail";
+
+//-------------------------- TEST MAIL ----------------------
+
+
 // Connect Heroku Database
 // extract(parse_url($_ENV["DATABASE_URL"]));
 // var_dump("user=$user password=$pass host=$host dbname=" . substr($path, 1));

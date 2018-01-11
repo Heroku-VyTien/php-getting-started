011ebe884cf01d1cd824c52632ed1cf2d4fc94f4
Tien Nguyen <mac-l011@mac-l011s-MacBook-Pro-2.local>
2018-01-11 15:44:19 +0700

    update

--- web/index.php
+++ web/index.php
@@ -5,36 +5,73 @@ require('../vendor/autoload.php');
 
 //-------------------------- TEST MAIL ----------------------
 
-function Send_Mail($to,$subject,$body)
-{
-    require '/PHPMailer/class.phpmailer.php';
-    $from = "nguyen.tien@mulodo.com";
-    $mail = new PHPMailer();
-    $mail->IsSMTP(true); // SMTP
-    $mail->SMTPAuth   = true;  // SMTP authentication
-    $mail->Mailer = "smtp";
-    $mail->Host= "tls://email-smtp.us-east-1.amazonaws.com"; // Amazon SES
-    $mail->Port = 465;  // SMTP Port
-    $mail->Username = "AKIAIM3HS7UUXEQ4GTPA";  // SMTP  Username
-    $mail->Password = "AqF6E+ZsiPdZd+mvMhkRgiQvQH7tGH9Ky5POVQCG63qX";  // SMTP Password
-    $mail->SetFrom($from, 'Tien nguyen');
-    $mail->AddReplyTo($from,'Technical Support');
-    $mail->Subject = $subject;
-    $mail->MsgHTML($body);
-    $address = $to;
-    $mail->AddAddress($address, $to);
-    if(!$mail->Send())
-        return false;
-    else
-        return true;
+require '/PHPMailer/class.phpmailer.php';
+
+// Instantiate a new PHPMailer 
+$mail = new PHPMailer;
+
+// Tell PHPMailer to use SMTP
+$mail->isSMTP();
+
+// Replace sender@example.com with your "From" address. 
+// This address must be verified with Amazon SES.
+$mail->setFrom('nguyen.tien@mulodo.com', 'Sender Name');
+
+// Replace recipient@example.com with a "To" address. If your account 
+// is still in the sandbox, this address must be verified.
+// Also note that you can include several addAddress() lines to send
+// email to multiple recipients.
+$mail->addAddress('vytien@gmail.com', 'Recipient Name');
+
+// Replace smtp_username with your Amazon SES SMTP user name.
+$mail->Username = 'AKIAIM3HS7UUXEQ4GTPA';
+
+// Replace smtp_password with your Amazon SES SMTP password.
+$mail->Password = 'AqF6E+ZsiPdZd+mvMhkRgiQvQH7tGH9Ky5POVQCG63qX';
+    
+// Specify a configuration set. If you do not want to use a configuration
+// set, comment or remove the next line.
+$mail->addCustomHeader('X-SES-CONFIGURATION-SET', 'ConfigSet');
+ 
+// If you're using Amazon SES in a region other than US West (Oregon), 
+// replace email-smtp.us-west-2.amazonaws.com with the Amazon SES SMTP  
+// endpoint in the appropriate region.
+$mail->Host = 'email-smtp.us-east-1.amazonaws.com';
+
+// The subject line of the email
+$mail->Subject = 'Amazon SES test (SMTP interface accessed using PHP)';
+
+// The HTML-formatted body of the email
+$mail->Body = '<h1>Email Test</h1>
+    <p>This email was sent through the 
+    <a href="https://aws.amazon.com/ses">Amazon SES</a> SMTP
+    interface using the <a href="https://github.com/PHPMailer/PHPMailer">
+    PHPMailer</a> class.</p>';
+
+// Tells PHPMailer to use SMTP authentication
+$mail->SMTPAuth = true;
+
+// Enable TLS encryption over port 587
+$mail->SMTPSecure = 'tls';
+$mail->Port = 465; // 587
+
+// Tells PHPMailer to send HTML-formatted email
+$mail->isHTML(true);
+
+// The alternative email body; this is only displayed when a recipient
+// opens the email in a non-HTML email client. The \r\n represents a 
+// line break.
+$mail->AltBody = "Email Test\r\nThis email was sent through the 
+    Amazon SES SMTP interface using the PHPMailer class.";
+
+if(!$mail->send()) {
+    echo "Email not sent. " , $mail->ErrorInfo , PHP_EOL;
+} else {
+    echo "Email sent!" , PHP_EOL;
 }
 
-$to = 'vytien@gmail.com';
-$subject = 'Test mail from AWS';
-$body = 'test body';
-$rst = Send_Mail($to,$subject,$body);
-if ($rst) echo "MAIL :: Success";
-else echo "MAIL :: Fail";
+
+
 
 //-------------------------- TEST MAIL ----------------------
 
